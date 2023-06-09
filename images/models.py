cd4945fe5f76687a980f8e1263b4a0992a4a3ec4
Misha <mishagalko1@gmail.com>
2023-06-09 13:11:01 +0200

    first commit

---
+++ images/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+class Image(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created',on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,blank=True)
+    url = models.URLField(max_length=2000)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='images_liked',blank=True)
+    total_likes = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+            models.Index(fields=['-total_likes'])
+        ]
+        ordering = ['-created']
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id,self.slug])
+
+
+class Contact(models.Model):
+    user_from = models.ForeignKey('auth.User', related_name='rel_from_set', on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User', related_name='rel_to_set',on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created']
+
+    def __str__(self) -> str:
+        return f'{self.user_from} follows {self.user_to}'
+    
+user_model = get_user_model()
+user_model.add_to_class('followers', models.ManyToManyField('self',
+                                                             through=Contact,
+                                                             related_name='foolowers',
+                                                             symmetrical=False))
+
